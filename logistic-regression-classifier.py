ca46ee77a8e19c2990b1585d052b2a24da880ce1
Walid Daboubi <walid.daboubi@gmail.com>
2017-03-04 19:01:45 +0100

    sample labled data examples added - Code cleaned

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -1,13 +1,9 @@
-
 import numpy as np
 from sklearn import tree,linear_model
 import sys
 
 train_data_file=sys.argv[1]
 test_data_file=sys.argv[2]
-
-#train_data_file='labeled_data_train.csv'
-#test_data_file='labeled_data_train.csv'
 
 labeled_data_train = np.genfromtxt (train_data_file, delimiter=",")
 data_train=labeled_data_train[:,[0,1,2]]
@@ -17,27 +13,43 @@ labeled_data_test = np.genfromtxt (test_data_file, delimiter=",")
 data_test=labeled_data_test[:,[0,1,2]]
 label_test=labeled_data_test[:,3]
 
+#The classifier
 attack_classifier=tree.DecisionTreeClassifier()
 attack_classifier=attack_classifier.fit(data_train,label_train)
 
-# ---------------------
-
-h = .02  # step size in the mesh
-
-logreg = linear_model.LogisticRegression(C=1e5)
-
-logreg.fit(data_train,label_train)
-
-# ----------------
-
-#print test_data
-
 c=0
-predicted_count=0.
-fitted_count=0. #exactly predicted
-
+#predicted with the classifier
+predicted_count=0. 
+#exactly predicted
+fitted_count=0. 
 
 indices=[]
+
+
+
+
+correct_predictions=0.0
+
+for predicted in attack_classifier.predict(data_test):
+	if label_test[c]==predicted:
+		correct_predictions+=1
+	c+=1
+print str(correct_predictions)+'/'+str(c)
+
+
+
+occuracy=100*(correct_predictions/c)
+print occuracy
+
+
+c=0
+#predicted with the classifier
+predicted_count=0.
+#exactly predicted
+fitted_count=0.
+
+indices=[]
+
 
 for predicted in attack_classifier.predict(data_test):
 	if predicted==1:
@@ -54,10 +66,4 @@ print predicted_count
 print fitted_count
 
 print 100.0*(fitted_count/predicted_count)
-
 print indices
-
-#print attack_classifier.predict(data_test)
-
-with open("attack_decision_tree.dot", 'w') as f:
-	f = tree.export_graphviz(attack_classifier, out_file=f)
