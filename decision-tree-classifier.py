31d3c6925de6f7e904e82caaf40f6ad61e018958
Walid DABOUBI <walid.daboubi@gmail.com>
2017-11-25 19:48:14 +0100

    Code review

--- decision-tree-classifier.py
+++ decision-tree-classifier.py
@@ -5,23 +5,22 @@
 
 from utilities import *
 
-#Get training features and labeles
-training_features,traning_labels=get_data_details(traning_data)
+# Get training features and labeles
+training_features, traning_labels = get_data_details(traning_data)
 
-#Get testing features and labels
-testing_features,testing_labels=get_data_details(testing_data)
+# Get testing features and labels
+testing_features, testing_labels = get_data_details(testing_data)
 
-### DECISON TREE CLASSIFIER
+# DECISON TREE CLASSIFIER
 print "\n\n=-=-=-=-=-=-=- Decision Tree Classifier -=-=-=-=-=-=-=-\n"
 
-#Instanciate the classifier
-attack_classifier=tree.DecisionTreeClassifier()
+# Instanciate the classifier
+attack_classifier = tree.DecisionTreeClassifier()
 
-#Train the classifier
-attack_classifier=attack_classifier.fit(training_features,traning_labels)
+# Train the classifier
+attack_classifier = attack_classifier.fit(training_features,traning_labels)
 
-#get predections for the testing data
-predictions=attack_classifier.predict(testing_features)
+# Get predections for the testing data
+predictions = attack_classifier.predict(testing_features)
 
-print "The precision of the Decision Tree Classifier is: "+str(get_occuracy(testing_labels,predictions,1))+"%"
-
+print "The precision of the Decision Tree Classifier is: " + str(get_occuracy(testing_labels,predictions,1)) + "%"
